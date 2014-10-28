6d49a8a8010b3bbbe48eaa446532dcb4eb36c6c0
seongminwoo <seongminwoo@gmail.com>
2014-10-28 17:33:49 +0900

    first commit

---
+++ src/main/java/tprov/controller/MainController.java
@@ -0,0 +1,12 @@
+package tprov.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+}
